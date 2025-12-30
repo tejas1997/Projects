6c711af0b307ac551f13b1ef002028176bac83eb
tejas1997 <47468029+tejas1997@users.noreply.github.com>
2025-12-30 11:48:50 +0000

    added imports in Basetest instead of TestFlipkart

--- Project_new/Projects_new/TestFlipkart/src/main/java/BaseTest.java
+++ Project_new/Projects_new/TestFlipkart/src/main/java/BaseTest.java
@@ -1,3 +1,13 @@
+import org.openqa.selenium.By; // 2. Added Import
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest 
 {
     public WebDriver driver;
