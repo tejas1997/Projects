aae1a85be8d50b2566be5a37d37ec28ee2eca29e
tejas1997 <47468029+tejas1997@users.noreply.github.com>
2025-12-30 12:34:29 +0000

    folder changes

--- Project_new/Projects_new/TestFlipkart/src/test/java/BaseTest.java
+++ Project_new/Projects_new/TestFlipkart/src/test/java/BaseTest.java
@@ -1,4 +1,4 @@
-import org.openqa.selenium.By; // 2. Added Import
+import org.openqa.selenium.By; 
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
