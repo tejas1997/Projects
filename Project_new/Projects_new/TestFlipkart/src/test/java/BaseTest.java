3b9d074162b65ac3c1fbfe27573eaad429e71974
Tejas <tejasgowda431@gmail.com>
2025-12-31 00:17:53 +0530

    no changes

--- Project_new/Projects_new/TestFlipkart/src/test/java/BaseTest.java
+++ Project_new/Projects_new/TestFlipkart/src/test/java/BaseTest.java
@@ -1,11 +1,13 @@
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.time.Duration;
 
 public class BaseTest 
 {
