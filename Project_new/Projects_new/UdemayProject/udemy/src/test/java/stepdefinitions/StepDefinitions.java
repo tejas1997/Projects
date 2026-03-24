a1a798312ff4de3ba22b3ec81e31684d2dd91bad
Tejas Gowda <tejas@ztss.pvt>
2026-03-24 19:48:37 +0530

    written code for fibonacci series

--- Project_new/Projects_new/UdemayProject/udemy/src/test/java/stepdefinitions/StepDefinitions.java
+++ Project_new/Projects_new/UdemayProject/udemy/src/test/java/stepdefinitions/StepDefinitions.java
@@ -1,72 +1,74 @@
 package stepdefinitions;
 
 import java.io.File;
-import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-
-import static driver.DriverFactory.getDriver;
+import java.time.Duration;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import driver.DriverFactory;
+public class StepDefinitions {
 
-public class StepDefinitions 
-{
-
-    private WebDriver driver = getDriver();
+    private WebDriver driver;
 
     @Given("I access the WebDriver University Contact Us Page")
     public void i_access_the_WebDriver_University_Contact_Us_Page() {
 
-        driver.get("https://webdriveruniversity.com/Contact-Us/contactus.html");
+        DriverFactory.getDriver().get("https://webdriveruniversity.com/Contact-Us/contactus.html");
     }
 
     @When("I enter a firstname {word}")
     public void i_enter_a_first_name(String firstName) {
 
-        driver.findElement(By.name("first_name")).sendKeys(firstName);
+        DriverFactory.getDriver().findElement(By.name("first_name")).sendKeys(firstName);
     }
 
     @And("I enter a lastname {word}")
     public void i_enter_a_last_name(String lastName) {
 
-        driver.findElement(By.name("last_name")).sendKeys(lastName);
+        DriverFactory.getDriver().findElement(By.name("last_name")).sendKeys(lastName);
     }
 
     @And("I enter an email address {string}")
     public void i_enter_an_email_address(String emailAddress) {
-        driver.findElement(By.name("email")).sendKeys(emailAddress);
+        DriverFactory.getDriver().findElement(By.name("email")).sendKeys(emailAddress);
     }
 
     @And("I enter a comment {string}")
     public void i_enter_a_comment(String comment) {
 
-        driver.findElement(By.name("message")).sendKeys(comment);
+        DriverFactory.getDriver().findElement(By.name("message")).sendKeys(comment);
     }
 
     @And("I click on the submit button")
     public void i_click_on_the_submit_button() {
 
-        driver.findElement(By.xpath("//input[@value='SUBMIT']")).click();
+        DriverFactory.getDriver().findElement(By.xpath("//input[@value='SUBMIT']")).click();
     }
 
     @Then("I should be presented with a submission message {string}")
     public void i_should_be_presented_with_a_submission_message(String expectedMessage) throws Exception {
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(DriverFactory.getDriver(), Duration.ofSeconds(10));
         WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='contact_reply']/h1")));
 
         Assert.assertEquals(successMessage.getText(), expectedMessage);
 
-        TakesScreenshot ts = (TakesScreenshot) driver;
+        TakesScreenshot ts = (TakesScreenshot) DriverFactory.getDriver();
         File source = ts.getScreenshotAs(OutputType.FILE);
 
         String filename = "screenshot_" + System.currentTimeMillis() + ".png";
