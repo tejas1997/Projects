68daa1c966d0c970dd539fd39f50e983d6b6206f
tejas431 <tejasgowda431@gmail.com>
2026-05-15 18:37:47 +0530

    written code for arraylist example

---
+++ Project_new/ArrayLists.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayLists{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Object> list = new ArrayList<>();
+        int listSize = sc.nextInt();
+        int sum = 0;
+        System.out.println("Enter " + listSize + " elements: ");
+        for(int i=0; i<listSize; i++){
+        list.add(sc.nextInt());
+        }
+        System.out.println(list);
+        for(int i=0; i<listSize; i++){
+            sum += (int) list.get(i);
+        }
+        System.out.println("Sum: " + sum);
+        sc.close();
+    }
+}
